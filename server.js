5be5d5c8fc2db3801d6afed8e5c583a8f4087e5c
RoyalKode <Madsoestrem@gmail.com>
2017-10-11 20:20:16 +0200

    my server proposal

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+
+//SET PORT
+var port = 3000;
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+//SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(process.env.PORT || port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
