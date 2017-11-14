0914b6403f0f8eaa91a2980428c1859cf01a2979
RoyalKode <Madsoestrem@gmail.com>
2017-11-14 13:05:59 +0100

    Changes

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// ROUTES
-var routes = require('./routes/index');
-var users = require('./routes/users');
+app.use(function(req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();
+})
 
-// INIT APP
-var app = express();
-
-// VIEW ENGINE-----DEFAULT-LAYOUT-----LAYOUT
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
-
-// BODY-PARSER MIDDLEWARE
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// SET STATIC FOLDER
-app.use(express.static(path.join(__dirname, 'public')));
-
-// GLOBAL VARIABLES
-app.use('/', routes);
+var users = require('./users');
 app.use('/users', users);
 
 
-// APP LISTEN
+app.get('/', function(req, res) {
+    res.send('LANDINGPAGE');
+});
+
 app.listen(process.env.PORT || 3000, function (){
   console.log('server is running on port %d in %s mode', this.address().port, app.settings.env);
 });
